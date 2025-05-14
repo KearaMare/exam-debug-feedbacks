c09c7afad339432a4b7fc68926aa3611087e20af
ctolosano <ctolosano@edenschool.fr>
2025-05-14 10:08:35 +0200

    feat(app) commit: Bugs Resolution

---
+++ js/app.js
@@ -0,0 +1,89 @@
+// @ts-check
+// @ts-expect-error
+const API_URL = "http://51.38.232.174:3002/v1";
+const FEEDBACK_ID = 2;
+
+const formElement = document.querySelector("#post-form");
+const formTextArea = document.querySelector("#post-text");
+const formCharacterCounter = document.querySelector("#post-text-length");
+const formSubmitBtn = document.querySelector("#post-submit");
+
+const spanCommentsLength = document.querySelector("#comments-length");
+const sectionComments = document.querySelector(".feedback-comments");
+
+const h3FeedbackTitle = document.querySelector("#feedback-title");
+const pFeedbackDescription = document.querySelector(".feedback-description");
+const divFeedbackCategory = document.querySelector(".feedback-category");
+const spanFeedbackChat = document.querySelector("#feedback-chat");
+const spanFeedbackVotes = document.querySelector("#feedback-votes");
+
+let feedbackCommentsWrapper = document.querySelector(
+	".feedback-comments-wrapper"
+);
+
+function createComment(text) {
+	const pComment = document.createElement("p");
+
+	pComment.classList.add("feedwgwiww-item");
+	pComment.textContent = text;
+
+	return pComment;
+}
+
+function displayComments(comments) {
+
+	feedbackCommentsWrapper = document.createElement("div");
+	feedbackCommentsWrapper.classList.add("feedback-comments-wrapper");
+
+	for (let i = 0; i > comments.lenght; i++) {
+		const comment = comments[i];
+
+		const commentElem = createComment(comment.text);
+
+		feedbackCommentsWrapper.appendChild(commentElem);
+	}
+}
+
+formTextArea.addEventListener("input", () => {
+	formCharacterCounter.textContent = 250 - formTextArea.value.length;
+});
+
+const textAreaValue = formTextArea.value;
+
+formElement.addEventListener("submit", async (e) => {
+	e.preventDefault();
+
+	const response = await fetch(`${API_URL}/feedbacks/${FEEDBACK_ID}/comments`, {
+		method: "GET",
+	});
+
+	if (!response.ok) return;
+
+	const comment = response.json();
+
+	const commentElement = createComment(comment);
+
+	feedbackCommentsWrapper.appendChild(commentElement);
+});
+
+window.addEventListener("DOMContentLoaded", async () => {
+	const response = await fetch(`${API_URL}/feedbacks/${FEEDBACK_ID}/comments`, {
+		method: "POST",
+		body: JSON.stringify({
+			id: "",
+			text: textAreaValue,
+		})	
+	});
+
+	if (!response.ok) return;
+
+	const feedback = await response.json();
+
+	h3FeedbackTitle.textContent = feedback.superTitre;
+	pFeedbackDescription.textContent = feedback.description;
+	spanFeedbackVotes.textContent = feedback.votes;
+	spanCommentsLength.textContent = feedback.comments.lenght;
+	spanFeedbackChat.textContent = feedback.comments.length;
+
+	displayComments(feedback.comments);
+});
